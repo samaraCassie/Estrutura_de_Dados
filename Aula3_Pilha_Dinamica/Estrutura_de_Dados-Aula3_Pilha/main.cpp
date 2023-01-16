199a73b026890d4e007c6bbe302974c06fd3d2a3
samaraCassie <samaracassie@outlook.com>
2023-01-16 06:55:36 -0300

    Create main.cpp

---
+++ Aula3_Pilha_Dinamica/Estrutura_de_Dados-Aula3_Pilha/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include "aula3.cpp"
+
+using namespace std;
+ 
+int main()
+{
+    pilhaDinamica pilha1; // Chama classe(pilha) e cria objeto(pilha1)
+    TipoItem prato;
+    int opcao, tamanho; 
+
+    cout << "<----------Vamos Lavar os Pratos---------->\n\n";
+
+    do
+    {
+        cout << "\n[0] Abandonar a pia\n";
+        cout << "[1] Colocar prato sujo para lavar\n";
+        cout << "[2] Lavar prato\n";
+        cout << "[3] Ver quais são os pratos sujos\n"; // Tentar criar uma pilha para os pratos limpos
+        cin >> opcao;
+
+        if(opcao == 1)
+        {
+            cout << "\nQue prato eh esse?\n";
+            cin >> prato;
+            pilha1.inserir(prato);
+        } else if (opcao == 2)
+        {
+            prato = pilha1.remover();
+            if (prato != 0)
+            {
+                cout << "\nPrato lavado: " << prato << endl;
+            }else
+            {
+                cout << " ";
+            }
+        } else if (opcao == 3)
+        {
+            pilha1.imprimir();
+        }
+    } while (opcao != 0 && opcao < 4);
+}
