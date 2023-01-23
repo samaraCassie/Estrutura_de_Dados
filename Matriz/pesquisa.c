791a332dde8ab615b8b8ace9cfc34fca4b5a872a
samaraCassie <samaracassie@outlook.com>
2023-01-23 10:41:04 -0300

    Create pesquisa.c

---
+++ Matriz/pesquisa.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void main()
+{
+   char descricao[50], pesquisa[50];
+   fflush(stdin);
+   printf("descricao do produto..: ");
+   gets(descricao);
+   fflush(stdin);
+   printf("substring de pesquisa.: ");
+   gets(pesquisa);
+   char* ret = strstr(descricao,pesquisa);
+   if (ret==0) {
+      printf("Nao corresponde\n");
+   }
+   else {
+      printf("Corresponde\n");
+      printf(" %s", descricao);
+   }
+}
+3276-9616
+
