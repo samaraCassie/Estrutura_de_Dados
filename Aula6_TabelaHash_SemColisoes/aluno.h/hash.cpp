63860a066feafa7c9ce3861a933a5268c405501a
samaraCassie <samaracassie@outlook.com>
2023-02-01 00:01:24 -0300

    construtor

--- Aula6_TabelaHash_SemColisoes/aluno.h/hash.cpp
+++ Aula6_TabelaHash_SemColisoes/aluno.h/hash.cpp
@@ -7,3 +7,10 @@ using namespace std;
     {
         return (aluno.obterRa() % max_posicoes);
     }
+    Hash::Hash(int tam_vetor, int max)
+    {
+        quant_itens = 0;
+        max_itens = max;
+        max_posicoes = tam_vetor;
+        estrutura = new Aluno[tam_vetor];
+    }
